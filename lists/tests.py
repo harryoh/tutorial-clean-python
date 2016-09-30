6684534816e8e3bb41d39dbe1309db3322010c0b
HoeGeun Oh <hoegeun.oh@rgpkorea.co.kr>
2016-10-01 02:00:22 +0900

    url mapping and temporary view

--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,15 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def teste_home_page_returns_corrent_heml(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>, response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
