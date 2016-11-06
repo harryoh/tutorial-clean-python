a8496046798f25bb89dbf7ba234836e164e3ba1a
harryoh <harry@5004.pe.kr>
2016-11-07 02:11:52 +0900

    Add model test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,7 +8,7 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         self.browser.find_element_by_id('id_new_item').send_keys('우유 사기\n')
         self.check_for_row_in_list_table('1: 우유 사기')
@@ -17,7 +17,7 @@ class ItemValidationTest(FunctionalTest):
 
         self.check_for_row_in_list_table('1: 우유 사기')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         self.browser.find_element_by_id('id_new_item').send_keys('tea 만들기\n')
         self.check_for_row_in_list_table('1: 우유사기')
